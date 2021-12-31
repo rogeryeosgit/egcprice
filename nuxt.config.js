1a59e86d9f9f0368696f7882594bccd69c6fe7da
RogerY <roger@geekshacking.com>
2021-12-31 14:16:01 +0800

    title change

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: 'EverGrow Coin ($EGC) Prices on Exchanges',
+    title: 'EverGrow Coin ($EGC) Prices on Exchanges',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
